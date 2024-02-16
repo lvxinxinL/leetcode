2b4a0e81ac5e15378bd58b9c0a5287aff6eed78a
Ghost <2089049079@qq.com>
2024-02-16 15:13:51 +0800

    add 438. 找到字符串中所有字母异位词

---
+++ hot100/sliding_window/FindAnagramsSolution.java
@@ -0,0 +1,45 @@
+package hot100.sliding_window;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 438. 找到字符串中所有字母异位词
+ * tips: 判断以异位词只需要维护每个字符的数量与目标串进行比较即可，不需要一个个遍历匹配！
+ * @author 乐小鑫
+ * @version 1.0
+ * @Date 2024-02-16-15:12
+ */
+public class FindAnagramsSolution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int sLen = s.length(), pLen = p.length();
+
+        if (sLen < pLen) {
+            return new ArrayList<Integer>();
+        }
+
+        List<Integer> ans = new ArrayList<Integer>();
+        int[] sCount = new int[26];
+        int[] pCount = new int[26];
+        for (int i = 0; i < pLen; ++i) {
+            ++sCount[s.charAt(i) - 'a'];
+            ++pCount[p.charAt(i) - 'a'];
+        }
+
+        if (Arrays.equals(sCount, pCount)) {
+            ans.add(0);
+        }
+
+        for (int i = 0; i < sLen - pLen; ++i) {
+            --sCount[s.charAt(i) - 'a'];
+            ++sCount[s.charAt(i + pLen) - 'a'];
+
+            if (Arrays.equals(sCount, pCount)) {
+                ans.add(i + 1);
+            }
+        }
+
+        return ans;
+    }
+}
